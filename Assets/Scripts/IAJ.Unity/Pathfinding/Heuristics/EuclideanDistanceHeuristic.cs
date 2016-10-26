a02760879277f5a0bad15792e0a1fb0b56659c6f
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-10-27 00:46:59 +0100

    Euclidean Distance Heuristic
    Copied from previous lab class

---
+++ Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/EuclideanDistanceHeuristic.cs
@@ -0,0 +1,17 @@
+﻿using RAIN.Navigation.Graph;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
+{
+    public class EuclideanDistanceHeuristic : IHeuristic
+    {
+        public EuclideanDistanceHeuristic()
+        {
+        }
+
+        public float H(NavigationGraphNode node, NavigationGraphNode goalNode)
+        {
+            return (node.Position - goalNode.Position).magnitude;
+        }
+    }
+}
