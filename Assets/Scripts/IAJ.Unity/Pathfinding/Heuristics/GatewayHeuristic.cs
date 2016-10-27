3c0a804e38b6dd1584766097172aff2cdf5e305b
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-10-27 14:38:49 +0100

    Added Gateway Heuristic :door:

--- Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/GatewayHeuristic.cs
+++ Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/GatewayHeuristic.cs
@@ -16,8 +16,28 @@ namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
         public float H(NavigationGraphNode node, NavigationGraphNode goalNode)
         {
             //for now just returns the euclidean distance
-            return EuclideanDistance(node.LocalPosition, goalNode.LocalPosition);
-            //TODO implement this properly
+            Cluster StartCluster = ClusterGraph.Quantize(node);
+            Cluster GoalCluster = ClusterGraph.Quantize(goalNode);
+
+            if (StartCluster.Equals(GoalCluster))
+                return EuclideanDistance(node.LocalPosition, goalNode.LocalPosition);
+            else
+            {
+                float min = float.MaxValue;
+                foreach(Gateway g1 in StartCluster.gateways)
+                {
+                    foreach(Gateway g2 in GoalCluster.gateways)
+                    {
+                        float h = 
+                            EuclideanDistance(node.LocalPosition, g1.center) + 
+                            ClusterGraph.gatewayDistanceTable[g1.id].entries[g2.id].shortestDistance + 
+                            EuclideanDistance(g2.center, goalNode.LocalPosition);
+                        if (h < min)
+                            min = h;
+                    }
+                }
+                return min;
+            }
         }
 
         public float EuclideanDistance(Vector3 startPosition, Vector3 endPosition)
